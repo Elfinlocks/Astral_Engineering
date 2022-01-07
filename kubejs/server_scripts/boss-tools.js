407c7f4bc9821d41a2507d5596294555091fc0c9
Elfinlocks <toatahu@bellsouth.net>
2022-01-07 15:21:27 -0500

    Made Earth Talisman craftable

--- kubejs/server_scripts/boss-tools.js
+++ kubejs/server_scripts/boss-tools.js
@@ -1,6 +1,5 @@
-events.listen('block.tags', function (event) {
+events.listen('fluid.tags', function (event) {
 	event.get('boss_tools:vehicle_fuel').remove('immersiveengineering:biodiesel')
 	event.get('boss_tools:vehicle_fuel').remove('immersivepetroleum:diesel')
 	event.get('boss_tools:vehicle_fuel').remove('thermal:refined_fuel')
 })
-
