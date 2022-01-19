d97c7d95d69c9fbb441465255c83e035a3f1d79a
Elfinlocks <toatahu@bellsouth.net>
2022-01-18 19:17:49 -0500

    Tagged sunglasses as a glasses bauble

--- kubejs/server_scripts/misc_tagging.js
+++ kubejs/server_scripts/misc_tagging.js
@@ -156,4 +156,5 @@ events.listen('item.tags', function (event) {
 	event.get('forge:workbench').add('minecraft:crafting_table')
 	event.get('chisel:marble').add('explorercraft:marble')
     event.get('chisel:basalt').add('explorercraft:basalt')
+	event.get('curios:glasses').add('bountifulbaubles:sunglasses')
 })
