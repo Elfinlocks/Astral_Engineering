ced951604bff3e3e900da00811a155b7ea3b9a46
Elfinlocks <toatahu@bellsouth.net>
2022-01-26 18:15:57 -0500

    Buffing IE Cables and adding tooltips

---
+++ kubejs/client_scripts/client.js
@@ -0,0 +1,8 @@
+onEvent('item.tooltip', tooltip => {
+	tooltip.add("immersiveengineering:ore_aluminum", [`§7Impure Aluminum - it will need electrolysis to purify it, in place of standard smelting.`]);
+	tooltip.add("immersiveengineering:wirecoil_copper", [`§7Maximum transfer rate of 2,048 Fe/T.`]);
+	tooltip.add("immersiveengineering:wirecoil_copper_ins", [`§7Maximum transfer rate of 2,048 Fe/T.`]);
+	tooltip.add("immersiveengineering:wirecoil_electrum", [`§7Maximum transfer rate of 8,192 Fe/T.`]);
+	tooltip.add("immersiveengineering:wirecoil_electrum_ins", [`§7Maximum transfer rate of 8,192 Fe/T.`]);
+	tooltip.add("immersiveengineering:wirecoil_steel", [`§7Maximum transfer rate of 32,768 Fe/T. Be careful of electric shock!`]);
+})
