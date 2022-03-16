b9cff7832d30af7769ab978fce85bfeb19f83fa1
Elfinlocks <toatahu@bellsouth.net>
2022-03-16 17:34:58 -0400

    Andesite Alloy Rework part 1

--- kubejs/startup_scripts/block_and_item_registry.js
+++ kubejs/startup_scripts/block_and_item_registry.js
@@ -13,4 +13,11 @@ onEvent('item.registry', event => {
 	event.create('rocket_salvage_1').displayName('Tier 1 Rocket Salvage')
 	event.create('rocket_salvage_2').displayName('Tier 2 Rocket Salvage')
 	event.create('impure_vinteum').displayName('Impure Vinteum Dust')
+	event.create('andesite_coated_iron').displayName('Andesite Coated Iron')
+	event.create('andesite_dust').displayName('Andesite Dust')
+})
+
+onEvent('fluid.registry', event => {
+	let colors = [0xCBE827, 0xAEE827, 0x68E827, 0x27E86E, 0x27E8B1, 0x27DEE8, 0x27B5E8, 0x2798E8, 0x2778E8, 0x2748E8]
+	event.create('matrix').displayName(`Molten Ultimate Alloy`).textureStill('kubejs:fluid/ultimate_still').textureFlowing('kubejs:fluid/ultimate_flowing').bucketColor(colors[0])
 })
